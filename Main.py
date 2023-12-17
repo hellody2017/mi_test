6753ea34f9e9cf818aa6370f29cdf95f60460017
hellody2017 <148761845+hellody2017@users.noreply.github.com>
2023-12-17 14:52:00 +0900

    Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,5 @@
-# st_pages 모듈에서 가져올 수 있는 코드가 없으므로 예시로 직접 정의하는 경우
-class Page:
-    def __init__(self, title, content, icon):
-        self.title = title
-        self.content = content
-        self.icon = icon
-
-    def show_content(self):
-        print(self.content)
-
-# 나머지 코드는 그대로 유지
 import streamlit as st
+from st_pages import Page
 
 def show_pages(pages):
     return [page.title for page in pages]
@@ -31,19 +21,15 @@ pages = [
     Page("miapp.py", "직업탐구", ":writing_hand:"),
 ]
 
-#selected_page = st.sidebar.selectbox("페이지 선택", show_pages(pages))
+# 직접 선택된 페이지를 설정
+selected_page = "Main.py"
 
+# 선택된 페이지 내용을 표시
 show_content(selected_page, pages)
 
 st.header("주의사항:warning:")
 image3 = "./image/warning.jpg"
 st.image(image3, use_column_width=True)
-
-
-
-
-
-
 
 
 
